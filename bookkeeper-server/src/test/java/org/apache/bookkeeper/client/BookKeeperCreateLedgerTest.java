e0ec0b6a6f04d092a769b941a0f0540366ee0afe
mazzucchi-andrea <mazzucchiandrea@gmail.com>
2025-04-17 12:18:10 +0200

    null password tests

--- bookkeeper-server/src/test/java/org/apache/bookkeeper/client/BookKeeperCreateLedgerTest.java
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/client/BookKeeperCreateLedgerTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.params.provider.MethodSource;
 import java.util.Arrays;
 import java.util.Collection;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 class BookKeeperCreateLedgerTest extends BookKeeperClusterTestCase {
 
@@ -19,33 +19,42 @@ class BookKeeperCreateLedgerTest extends BookKeeperClusterTestCase {
     private static Collection<Object[]> provideTestData() {
         byte[] mediumPassword = new byte[8];
         byte[] longPassword = new byte[256];
-        Arrays.fill(mediumPassword, (byte) 0x2A);
-        Arrays.fill(longPassword, (byte) 0x3F);
+        Arrays.fill(mediumPassword, (byte) 0x2A); // Fill with repeated value
+        Arrays.fill(longPassword, (byte) 0x3F); // Fill with repeated value
 
         return Arrays.asList(new Object[][]{
-                {BookKeeper.DigestType.MAC, new byte[]{}},
-                {BookKeeper.DigestType.MAC, new byte[]{0x01}},
-                {BookKeeper.DigestType.MAC, mediumPassword},
-                {BookKeeper.DigestType.MAC, longPassword},
-                {BookKeeper.DigestType.MAC, new byte[]{(byte) 0xFF}},
+                {BookKeeper.DigestType.MAC, new byte[]{}}, // Empty password
+                {BookKeeper.DigestType.MAC, new byte[]{0x01}}, // Single byte password
+                {BookKeeper.DigestType.MAC, mediumPassword}, // Medium length password
+                {BookKeeper.DigestType.MAC, longPassword}, // Long password
+                {BookKeeper.DigestType.MAC, new byte[]{(byte) 0xFF}}, // Password with a special character
 
-                {BookKeeper.DigestType.CRC32, new byte[]{}},
-                {BookKeeper.DigestType.CRC32, new byte[]{0x01}},
-                {BookKeeper.DigestType.CRC32, mediumPassword},
-                {BookKeeper.DigestType.CRC32, longPassword},
-                {BookKeeper.DigestType.CRC32, new byte[]{(byte) 0xFF}},
+                {BookKeeper.DigestType.CRC32, new byte[]{}}, // Empty password
+                {BookKeeper.DigestType.CRC32, new byte[]{0x01}}, // Single byte password
+                {BookKeeper.DigestType.CRC32, mediumPassword}, // Medium length password
+                {BookKeeper.DigestType.CRC32, longPassword}, // Long password
+                {BookKeeper.DigestType.CRC32, new byte[]{(byte) 0xFF}}, // Password with a special character
 
-                {BookKeeper.DigestType.CRC32C, new byte[]{}},
-                {BookKeeper.DigestType.CRC32C, new byte[]{0x01}},
-                {BookKeeper.DigestType.CRC32C, mediumPassword},
-                {BookKeeper.DigestType.CRC32C, longPassword},
-                {BookKeeper.DigestType.CRC32C, new byte[]{(byte) 0xFF}},
+                {BookKeeper.DigestType.CRC32C, new byte[]{}}, // Empty password
+                {BookKeeper.DigestType.CRC32C, new byte[]{0x01}}, // Single byte password
+                {BookKeeper.DigestType.CRC32C, mediumPassword}, // Medium length password
+                {BookKeeper.DigestType.CRC32C, longPassword}, // Long password
+                {BookKeeper.DigestType.CRC32C, new byte[]{(byte) 0xFF}}, // Password with a special character
 
-                {BookKeeper.DigestType.DUMMY, new byte[]{}},
-                {BookKeeper.DigestType.DUMMY, new byte[]{0x01}},
-                {BookKeeper.DigestType.DUMMY, mediumPassword},
-                {BookKeeper.DigestType.DUMMY, longPassword},
-                {BookKeeper.DigestType.DUMMY, new byte[]{(byte) 0xFF}}
+                {BookKeeper.DigestType.DUMMY, new byte[]{}}, // Empty password
+                {BookKeeper.DigestType.DUMMY, new byte[]{0x01}}, // Single byte password
+                {BookKeeper.DigestType.DUMMY, mediumPassword}, // Medium length password
+                {BookKeeper.DigestType.DUMMY, longPassword}, // Long password
+                {BookKeeper.DigestType.DUMMY, new byte[]{(byte) 0xFF}} // Password with a special character
+        });
+    }
+
+    private static Collection<Object[]> provideInvalidTestData() {
+        return Arrays.asList(new Object[][]{
+                {BookKeeper.DigestType.MAC, null}, // Null password
+                {BookKeeper.DigestType.CRC32, null}, // Null password
+                {BookKeeper.DigestType.CRC32C, null}, // Null password
+                {BookKeeper.DigestType.DUMMY, null} // Null password
         });
     }
 
@@ -59,6 +68,19 @@ class BookKeeperCreateLedgerTest extends BookKeeperClusterTestCase {
             try (LedgerHandle ledgerHandle = bookKeeper.createLedger(digestType, password)) {
                 assertNotNull(ledgerHandle);
             }
+        }
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideInvalidTestData")
+    void testCreateLedgerInvalidPassword(BookKeeper.DigestType digestType, byte[] password) {
+        ClientConfiguration conf = new ClientConfiguration();
+        conf.setMetadataServiceUri(zkUtil.getMetadataServiceUri());
+
+        try (BookKeeper bookKeeper = new BookKeeper(conf)) {
+            assertThrows(Exception.class, () -> bookKeeper.createLedger(digestType, password));
+        } catch (Exception e) {
+            fail("Exception should not be thrown here: " + e.getMessage());
         }
     }
 }
